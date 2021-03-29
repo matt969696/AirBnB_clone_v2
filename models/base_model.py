fa6338a223d13ffe8d89e3c50ea65b5d9ce86b00
matt969696 <matsce2020@gmail.com>
2021-03-29 14:59:06 +0000

    fix() : add copy in str method

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        newDict = self.__dict__
+        newDict = self.__dict__.copy()
         if '_sa_instance_state' in newDict:
             del newDict['_sa_instance_state']
         return '[{}] ({}) {}'.format(cls, self.id, newDict)
