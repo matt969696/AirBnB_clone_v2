4ca0317a25ecb413f4dabe8702a747d3658b4d0d
matt969696 <matsce2020@gmail.com>
2021-03-29 07:21:45 +0000

    feat() : update city & state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models import storage
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="delete", backref='state')
+
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            """Getter for cities when using FileStorage mode"""
+            ret = []
+            for c in storage.all(City):
+                if c.state_id == self.id:
+                    ret.append(c)
+            return ret
