f7887f02563aa63b9fb31340cf8241f238750981
fourbet <ofourbet@yahoo.fr>
2021-03-29 14:14:45 +0000

    undo: debug

--- models/base_model.py
+++ models/base_model.py
@@ -46,11 +46,8 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        print(dictionary)
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
-        print("-----------------------")
-        print(dictionary)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
