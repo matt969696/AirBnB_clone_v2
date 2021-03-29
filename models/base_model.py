2130e55b4a1f975df62cceea2522c6a3b84a7fd7
matt969696 <matsce2020@gmail.com>
2021-03-29 15:28:21 +0000

    fix() : changed __init__

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             for k, v in kwargs.items():
                 if k in ["created_at", "updated_at"]:
                     setattr(self, k,
